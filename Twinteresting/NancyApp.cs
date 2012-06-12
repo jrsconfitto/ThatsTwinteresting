94dabf06915ee1ecde5177a20a8619dd1e2c0ac2
James Sconfitto <jugglingnutcase@gmail.com>
2012-06-11 23:30:02 -0500

    First commit for the Hackathon

---
+++ Twinteresting/NancyApp.cs
@@ -0,0 +1,31 @@
+﻿using Nancy;
+using Nancy.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NancyCSharpWORK
+{
+    public class NancyApp : NancyModule
+    {
+        public NancyApp()
+        {
+            Get["/"] = _ => "hello world";
+
+            Get["/{what}"] = parameters =>
+            {
+                var what = parameters.what;
+                return "hello there " + what + "!";
+            };
+        }
+    }
+
+    public class CustomBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override DiagnosticsConfiguration DiagnosticsConfiguration
+        {
+            get { return new DiagnosticsConfiguration { Password = @"Hackathon" }; }
+        }
+    }
+}
---
+++ Twinteresting/Program.cs
@@ -0,0 +1,25 @@
+﻿using Nancy.Hosting.Wcf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace NancyCSharpWORK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Serving on http://localhost:1234/");
+
+            var host = new WebServiceHost(new NancyWcfGenericService(),
+                                          new Uri("http://localhost:1234/"));
+            host.AddServiceEndpoint(typeof (NancyWcfGenericService), new WebHttpBinding(), "");
+            host.Open();
+
+            Console.ReadKey();
+        }
+    }
+}
