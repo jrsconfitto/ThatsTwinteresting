90c75c73f0e6e65f7edb2b55ca9c803205f30149
James Sconfitto <jugglingnutcase@gmail.com>
2012-06-12 00:56:33 -0500

    Added the ability to send out a query

--- Twinteresting/NancyApp.cs
+++ Twinteresting/NancyApp.cs
@@ -1,4 +1,5 @@
 ﻿using Nancy;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,18 @@ namespace NancyCSharpWORK
         public NancyApp()
         {
             Get["/"] = _ => View["Views/index"];
+
+            Get["/{query}"] = parameters =>
+            {
+                var query = parameters.query;
+
+                // Send a message through the web api to my little Twitter reading service
+                var client = new RestClient("http://localhost:1111");
+                RestRequest queryRequest = new RestRequest(query, Method.POST);
+                client.Execute(queryRequest);
+
+                return "cool";
+            };
         }
     }
 }
